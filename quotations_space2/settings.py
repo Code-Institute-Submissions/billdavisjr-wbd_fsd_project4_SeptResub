71dcc23f15ca7043f75bdbad4936c69df4ed7f01
Bill Davis <bill.davis@gmail.com>
2021-05-23 06:20:36 +0000

    Removed secret key and debug settings to environment variables

--- quotations_space2/settings.py
+++ quotations_space2/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dfy@%+(i=hg*pd1gskjcrkx*wkkpe6)*09&yr_84f$n)8&dbu2'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['wbd-fsd-project4.herokuapp.com', 'localhost']
 
